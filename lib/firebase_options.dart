2c7c1659338668db372669f27986ce2abfdf12fe
aliyyilulvi <aliyyilulvi@gmail.com>
2026-07-18 23:03:34 +0300

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -7,10 +7,8 @@
 // Bu dosya, normalde `flutterfire configure` komutuyla otomatik üretilir;
 // biz burada bilgisayara hiçbir şey kurmadan, elle doldurabilmen için
 // hazır bir şablon bıraktık.
-
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
-
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (defaultTargetPlatform == TargetPlatform.android) {
@@ -20,12 +18,11 @@ class DefaultFirebaseOptions {
       'DefaultFirebaseOptions sadece Android için yapılandırıldı.',
     );
   }
-
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'BURAYA_apiKey_YAPISTIR',
-    appId: 'BURAYA_appId_YAPISTIR',
-    messagingSenderId: 'BURAYA_messagingSenderId_YAPISTIR',
-    projectId: 'BURAYA_projectId_YAPISTIR',
-    storageBucket: 'BURAYA_projectId.appspot.com_YAPISTIR',
+    apiKey: 'AIzaSyC8sABGwZ2sQO42u7LHOMZhHVTHWun2h94',
+    appId: '1:282977545553:android:89cd4f6da301f8e9437770',
+    messagingSenderId: '282977545553',
+    projectId: 'aile-cell',
+    storageBucket: 'aile-cell.firebasestorage.app',
   );
 }
